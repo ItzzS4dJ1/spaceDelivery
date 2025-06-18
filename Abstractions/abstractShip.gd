04b5f171a1eaaf55e12916b6d5e484c466d2a82b
ItzzS4dJ1 <faridmardonov94@gmail.com>
2025-06-19 00:07:21 +0300

    small changes, not worth mentioning
    nothing much, small ship impro (added pre-hyperspeed)

--- Abstractions/abstractShip.gd
+++ Abstractions/abstractShip.gd
@@ -4,13 +4,14 @@ var speedUpgAmount : float
 var _rotation = 0.0
 var rotTween = create_tween()
 var camTween = create_tween()
-
+var isPreHyperSpace : bool = false
 
 
 @export var HORIZONTAL_SPEED : float = 200.0
 @export var VERTICAL_SPEED : float = 300.0
 @export var BACKWARD_SPEED : float = 50.0
 @export var rotation_speed : float = TAU * 0.5
+@export var PREHYPERSPACE_SPEED : float = VERTICAL_SPEED * 2.5 #mostly Vertical speed times two and a half
 
 var speed = 400
 
@@ -23,7 +24,10 @@ func get_input():
 	if Input.is_action_just_pressed("in_s"):
 		speed = BACKWARD_SPEED
 	elif Input.is_action_just_pressed("in_w"):
-		speed = VERTICAL_SPEED
+		if isPreHyperSpace:
+			speed = PREHYPERSPACE_SPEED
+		else:
+			speed = VERTICAL_SPEED
 	rotation = (deg_to_rad(_rotation))
 	velocity = transform.x * Input.get_axis("in_s", "in_w") * speed
 
@@ -38,11 +42,19 @@ func _physics_process(delta):
 	
 
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("in_lshift"):
+		print("Shift pressed")
+		if not isPreHyperSpace:
+			isPreHyperSpace = true
+			print(isPreHyperSpace)
+		else:
+			isPreHyperSpace = false
+			print(isPreHyperSpace)
 	if camTween:
 		camTween.kill()
 	else: 
 		camTween.tween_property(self,"rotation", deg_to_rad(_rotation), 0.9)
 	if Input.is_action_pressed("in_d"):
-		_rotation -= 2.5
-	if Input.is_action_pressed("in_a"):
 		_rotation += 2.5
+	if Input.is_action_pressed("in_a"):
+		_rotation -= 2.5
